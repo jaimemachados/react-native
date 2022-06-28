264fc0c03d6f7ff8c359a8dce5904ff3e57bf71e
Jaime Machado <jotamachados@hotmail.com>
2022-06-28 19:42:14 -0300

    Trabalhando com imagem no ReactNative

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {View, Text, StyleSheet, SafeAreaView, StatusBar} from 'react-native';
+import {View, Image, StyleSheet, SafeAreaView, StatusBar} from 'react-native';
+import {withSubscription} from 'react-native/Libraries/LogBox/Data/LogBoxData';
 
 const colorGitHub = '#010409';
+const ImageProfileGitHub =
+  'https://avatars.githubusercontent.com/u/104790097?v=4';
 const App = () => {
   return (
     <SafeAreaView style={style.container}>
-      <StatusBar backgroundColor={'colorGitHub'} barStyle='light-content'/>
-        <View>
-          <Text style={style.text}>Hello Word!</Text>
-        </View>
+      <StatusBar backgroundColor={'colorGitHub'} barStyle="light-content" />
+      <View>
+        <Image
+          accessibilityLabel="Jaime Machado de terno preto e gravata vermelha"
+          style={style.avatar}
+          source={{uri: ImageProfileGitHub}}
+        />
+      </View>
     </SafeAreaView>
   );
 };
@@ -17,12 +24,14 @@ export default App;
 
 const style = StyleSheet.create({
   container: {
-    backgroundColor:'colorGitHub',
-    flex:1,//Expandir para a tela inteira
+    backgroundColor: 'colorGitHub',
+    flex: 1, //Expandir para a tela inteira
   },
-  text:{
-    fontSize:50,
-    fontWeight: 'bold',
-    color:'black',
-  }
+  avatar: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    borderColor: 'white',
+    borderWidth: 2,
+  },
 });
