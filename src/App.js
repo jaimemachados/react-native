606c67c89f4f1eb9bed5afe163b9e5c313dc2516
Jaime Machado <jotamachados@hotmail.com>
2022-06-28 17:55:43 -0300

    Trabalhandocom SafeAreaView

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, StatusBar} from 'react-native';
 
+const colorGitHub = '#010409';
 const App = () => {
-  return null;
+  return (
+    <SafeAreaView style={style.container}>
+      <StatusBar backgroundColor={'colorGitHub'} barStyle='light-content'/>
+        <View>
+          <Text style={style.text}>Hello Word!</Text>
+        </View>
+    </SafeAreaView>
+  );
 };
 
 export default App;
+
+const style = StyleSheet.create({
+  container: {
+    backgroundColor:'colorGitHub',
+    flex:1,//Expandir para a tela inteira
+  },
+  text:{
+    fontSize:50,
+    fontWeight: 'bold',
+    color:'black',
+  }
+});
